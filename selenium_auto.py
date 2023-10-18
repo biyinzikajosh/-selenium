f314d71be013069fe06f3c3763cd2b903b24c5dd
biyinzikajosh <38345452+biyinzikajosh@users.noreply.github.com>
2023-10-18 17:21:46 +0300

    Update selenium_auto.py

--- selenium_auto.py
+++ selenium_auto.py
@@ -6,17 +6,17 @@ from selenium.webdriver.chrome.options import Options # Allows you to configure 
 chrome_driver_path = "/Users/philippmuellauer/Development/chromedriver" # Setting the path to the Chrome webdriver
 
 # keeps chrome open
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+chrome_options = webdriver.ChromeOptions() # Creates an instance or object from ChromeOptions class. This helps in configuring the Chrome WebDriver
+chrome_options.add_experimental_option("detach", True) # Enables continous interaction with the browser even after the script has ended
 
 # driver = webdriver.Chrome(executable_path=chrome_driver_path)
-# driver = webdriver.Chrome()
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome() # Creating a Chrome WebDriver instance
+driver = webdriver.Chrome(options=chrome_options) # Allows you to pass custom configurations and settings for the Chrome WebDriver
 
-def test_eight_components():
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-    title = driver.title
-    assert title == "Web form"
+def test_eight_components(): # Defining the test_eight_components() function
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html") # Opens the URL in the browser
+    title = driver.title # Retrieves the page title
+    assert title == "Web form" # Asserts that the page title is a Web form
     driver.implicitly_wait(0.5)
     text_box = driver.find_element(by=By.NAME, value="my-text")
     submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
